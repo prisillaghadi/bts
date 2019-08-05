bff56ab61326df9c8b7dfe0e760a7feeb16ac64e
prisillaghadi <52330243+prisillaghadi@users.noreply.github.com>
2019-08-05 15:20:03 +0530

    Add files via upload

---
+++ src/app/main-content/accordian/accordian.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accordian',
+  templateUrl: './accordian.component.html',
+  styleUrls: ['./accordian.component.scss']
+})
+export class AccordianComponent implements OnInit {
+  step = 0;
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
